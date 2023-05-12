e8ea6dbaaa43ecff0551af1dc62e92be4cf848ee
Alejandro Lozano Aparicio <alex2001lozano@gmail.com>
2023-05-12 23:38:42 +0200

    feat init

--- hack2023-front/src/index.js
+++ hack2023-front/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PhotoInfoSection } from './components/photoInfoSection/photoInfoSection';
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <PhotoInfoSection
+            src={
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Latin_letter_A_with_acute.svg/800px-Latin_letter_A_with_acute.svg.png'
+            }
+            alt={''}
+        />
+    </React.StrictMode>
 );
-
