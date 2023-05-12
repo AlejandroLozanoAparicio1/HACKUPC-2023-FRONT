dbc4dc363e70903ddb0c301bdba76ae9a18224ff
Alejandro Lozano Aparicio <alex2001lozano@gmail.com>
2023-05-13 01:11:51 +0200

    feat button

--- hack2023-front/src/components/shared/Button/button.js
+++ hack2023-front/src/components/shared/Button/button.js
@@ -1,11 +1,16 @@
-import styles from './button.module.scss'
+import styles from './button.module.scss';
 
-const Button = ({ label, onClick }) => {
-  return (
-    <button className={styles.button} onClick={onClick}>
-      {label}
-    </button>
-  );
+const Button = ({ label, onClick, disabled, type }) => {
+    return (
+        <button
+            disabled={disabled}
+            type={type}
+            className={styles.button}
+            onClick={onClick}
+        >
+            {label}
+        </button>
+    );
 };
 
 export default Button;
--- hack2023-front/src/index.js
+++ hack2023-front/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PhotoInfoSection } from './components/photoInfoSection/photoInfoSection';
+import Button from './components/shared/Button/button';
 import './index.module.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
