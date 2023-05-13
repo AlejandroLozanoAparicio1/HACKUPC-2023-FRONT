285bccc5b02f0d118aef5dd7b1d1ba232078d8ff
Alejandro Lozano Aparicio <alex2001lozano@gmail.com>
2023-05-13 03:59:55 +0200

    lkm

--- hack2023-front/src/components/app/app.jsx
+++ hack2023-front/src/components/app/app.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import ImageUploader from '../imageUploader/imageUploader';
 import { PhotoInfoSection } from '../photoInfoSection/photoInfoSection';
 import { toBase64 } from '../../services/toBase64';
+import { ImageUrlUploader } from '../imageUrlUploader/imageUrlUploader';
 
 export const App = () => {
     const [image, setImage] = useState(null);
@@ -12,7 +13,6 @@ export const App = () => {
             try {
                 //todo: url of the API
                 const src = toBase64(image);
-                console.log(src);
                 //const response = axios.post('/api/upload-image', a);
             } catch (error) {
                 console.log(error);
@@ -22,7 +22,8 @@ export const App = () => {
 
     return (
         <>
-            <ImageUploader image={image} setImage={setImage} />
+            <ImageUploader setImage={setImage} />
+            <ImageUrlUploader setImage={setImage} />
             {image && <PhotoInfoSection src={image} alt={''} />}
         </>
     );
