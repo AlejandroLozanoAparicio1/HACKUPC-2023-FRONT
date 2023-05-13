8251f426da52dcffbf4232fdd7857a3d0bd2240b
izancobl <izancordobilla@gmail.com>
2023-05-13 19:46:59 +0200

    feat: create routing

--- hack2023-front/src/components/header/header.jsx
+++ hack2023-front/src/components/header/header.jsx
@@ -1,15 +1,22 @@
+import { Link } from 'react-router-dom';
 import logo from './../../assets/logoHomeScope.png';
 import styles from './header.module.scss';
+import './header.css';
+import { useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
+    const location = useLocation();
+    console.log(location);
+    
     return (
         <div className={styles.header}>
             <img src={ logo } alt='logo'/>
             <div className={ styles.headerContents }>
-                <p className={ styles.home } >Home</p>
-                <p>Features</p>
-                <p>About us</p>
-                <p>Contact</p>
+                <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                <NavLink activeClassName="active" to="/improve-images">Improve Images</NavLink>
+                <NavLink activeClassName="active" to="/calculate-pricing">Calculate pricing</NavLink>
+                <NavLink activeClassName="active" to="/about">About us</NavLink>
             </div>
         </div>
     );
