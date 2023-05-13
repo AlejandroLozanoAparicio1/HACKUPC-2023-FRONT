09d309f208cf04c191d310c57028513da7135d5f
Alejandro Lozano Aparicio <alex2001lozano@gmail.com>
2023-05-13 17:05:35 +0200

    features as  tags

--- hack2023-front/src/components/infoCard/infoCard.jsx
+++ hack2023-front/src/components/infoCard/infoCard.jsx
@@ -6,13 +6,12 @@ const InfoCard = ({ roomType, features, score, caption, handleOnChange }) => {
         <div className={styles.infoCard}>
             <h2 className={styles.title}>{roomType}</h2>
             <p className={styles.caption}>{caption}</p>
-            <p className={styles.featuresTitle}>
-                Features
-            </p>
-            <ul>
+            <ul className={styles.features}>
                 {features.map((feature, index) => (
                     <li className={styles.feature} key={index}>
-                        {feature.label.replace(/_/g, " ").replace(/\b\w/g, l => l.toUpperCase())}
+                        {feature.label
+                            .replace(/_/g, ' ')
+                            .replace(/\b\w/g, (l) => l.toUpperCase())}
                     </li>
                 ))}
             </ul>
