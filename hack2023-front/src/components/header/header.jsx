647cb203291b37850f38c55cd667762c3e8210d7
izancobl <izancordobilla@gmail.com>
2023-05-13 03:51:51 +0200

    feat: landingPage and headerComponent done

--- hack2023-front/src/components/header/header.jsx
+++ hack2023-front/src/components/header/header.jsx
@@ -5,6 +5,12 @@ export const Header = () => {
     return (
         <div className={styles.header}>
             <img src={ logo } alt='logo'/>
+            <div className={ styles.headerContents }>
+                <p className={ styles.home } >Home</p>
+                <p>Features</p>
+                <p>About us</p>
+                <p>Contact</p>
+            </div>
         </div>
     );
 };
