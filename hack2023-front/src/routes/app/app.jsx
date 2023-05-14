7db838ffb83adfde0eb07fe7bb22ccb5c3e48cdb
izancobl <izancordobilla@gmail.com>
2023-05-14 09:02:40 +0200

    feat: final commit!

--- hack2023-front/src/routes/app/app.jsx
+++ hack2023-front/src/routes/app/app.jsx
@@ -1,5 +1,7 @@
 import { LandingPage } from '../../components/landingPage/landingPage';
 import { Header } from '../../components/header/header';
+import interiorDesign from './../../assets/landingpageImage.jpg';
+import interiorDesign2 from './../../assets/landingPageImage2.jpg';
 import styles from './app.module.scss';
 
 export const App = () => {
@@ -13,6 +15,10 @@ export const App = () => {
                     "Introducing HomeLens, the innovative photo improvement and analysis app for homeowners. Simply upload a photo of your house, and watch as HomeLens enhances its quality, bringing out the best in your home. Plus, receive valuable information and insights about your property, including its estimated value and other important details. With HomeLens, you'll see your home in a whole new way, and gain a better understanding of its potential. Try HomeLens today and take the first step towards unlocking your home's full potential."
                 }
             />
+            <div className={ styles.imageContainer }>
+                <img src={interiorDesign} alt="" />
+                <img src={interiorDesign2} alt="" />
+            </div>
         </div>
     );
 };
